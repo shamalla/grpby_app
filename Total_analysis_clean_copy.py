df8e523af0e109580baab8130caf3bea886a0276
Your Name <you@example.com>
2025-06-27 11:12:34 +0300

    Changs to make sure user can select columns to use in his merge

--- Total_analysis_clean_copy.py
+++ Total_analysis_clean_copy.py
@@ -131,9 +131,11 @@ if df1 is not None:
 
         #Columns to be included in our merge
         cols_inc = st.radio("Columns to include in the reconciliation",["Use all colums","Select specific_columns"])
+        default_df1 = list(set([merge_col_df1]) | set(df1.columns.tolist()))
+        default_df2 = list(set([merge_col_df2]) | set(df2.columns.tolist()))
         if cols_inc == "Select specific_columns":
-            cols_df1 = st.multiselect("Select columns to be included from df1",df1.columns.tolist(), default = [merge_col_df1], key = "col_d_f1")
-            cols_df2 = st.multiselect("Select columns to be included from File 2",df2.columns.tolist(), default = [merge_col_df2], key = "col_d_f2")
+            cols_df1 = st.multiselect("Select columns to be included from df1",options=df1.columns.tolist(), default=[col for col in default_df1 if col in df1.columns],key="col_d_f1")
+            cols_df2 = st.multiselect("Select columns to be included from File 2",doptions=df2.columns.tolist(),default=[col for col in default_df2 if col in df2.columns],key="col_d_f2")
             #Ensuring merged columns are included
             if merge_col_df1 not in cols_df1:
                 cols_df1.append(merge_col_df1)
