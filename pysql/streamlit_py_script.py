e4a1a10f8458b89fe8734dbdedc4c46e66617a5c
Your Name <you@example.com>
2025-07-08 12:00:00 +0300

    Plotting app

---
+++ pysql/streamlit_py_script.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+import pyodbc
+import plotly.express as px
+
+#setting the title of the page
+st.set_page_config(page_title = "SQL Server Dashboard")
+st.title("SQL Server Data viewer")
+
+server = st.text_input("SQL Server",value = "your_server_name")
+database = st.text_input("Database", value = "Database_name")
+user_name = st.text_input("User_name", value = "Your user_name")
+password = st.text_inpute("Password", type = "password")
+
+
+#A button is placed so that the app can only run if the credentials have been entrered
+if st.button("Connect and load your Data"):
+    try:
+        conn = pyodbc.connect(
+            f"DRIVER = {{ODBC Driver 17 for SQL Server}}"
+            f"SERVER = {server}"
+            f"DATABASE = {database}"
+            f"USER_NAME = {user_name}"
+            f"PASSWORD = {password}"
+        )
+
+        #sql query to load the data
+        query = st.text_area("Enter SQL Query", "SELECT TOP 100 * FROM your_table")
+
+        #Load the data from our question
+        df = pd.read_sql(query,conn)
+        st.success("Your data has been loaded successfully")
+
+        with st.expander("View your data"):
+            st.dataframe(df)
+
+        if not df.empty:
+            st.subheader("Plot options")
+        #Selecting numeric columns to use for plotting
+            numeric_columns = st.select_dtypes(include["number"]).column_tolist()
+            all_columns = df.columns.to_list()
+
+            x_cols = st.selectbox("Select X_axis columns", all_columns)
+            y_cols = st.selectbox("Select Y_axis columns", numeric_columns)
+
+            chart_type = st.radio("Select Chart type",["Line","Bar","Scatter","Histogram"])
+
+            if x_cols and y_cols:
+                if chart_type == "Line":
+                    fig = px.line(df, x=x_cols, y=y_cols, title=f"{y_cols} vs {x_cols}")
+                elif chart_type == "Bar":
+                    fig = px.bar(df, x = x_cols, y = y_cols, title = f"{y_cols} vs{x_cols}")
+                elif chart_type == "scatter":
+                    fig = px.scatter(df, x = x_cols, y = y_cols, title = f"{y_cols} vs{x_cols}")
+                elif chart_type == "Histogram":
+                    fig = px.histogram(df, x = x_cols, y = y_cols, title = f"{y_cols} vs{x_cols}")
+                st.plotly_chart(fig,use_container_width = True)
+            else:
+                st.warning("Please select both X and Y labels to plot.")
+    except Exception as e:
+        st.error(f"connection error or query failure\n{e}")    
