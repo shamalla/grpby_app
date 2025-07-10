4b2955be3436834a14f94f31cc140a3a85aec0b9
Your Name <you@example.com>
2025-07-10 09:10:06 +0300

    Working on the connection part

--- pysql/streamlitSQL_py_script.py
+++ pysql/streamlitSQL_py_script.py
@@ -18,10 +18,10 @@ password = st.text_input("Password", type = "password")
 if st.button("Connect and load your Data"):
     try:
         conn = pyodbc.connect(
-            f"DRIVER = {{ODBC Driver 17 for SQL Server}}"
-            f"SERVER = {server}"
-            f"DATABASE = {database}"
-            f"USER_NAME = {user_name}"
+            f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+            f"SERVER = {server};"
+            f"DATABASE = {database};"
+            f"USER_NAME = {user_name};"
             f"PASSWORD = {password}"
         )
 
